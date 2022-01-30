e805eda3aeae3a98e4d718c8300d87495b3d224a
PrashantBtkl <prashant44backup@gmail.com>
2022-01-30 18:08:55 +0530

    sleepsort using channels

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### Usage
+
 ```
-go run sleepsort.go 1 2 1 4 3 1
+go run sleepsort.go 7 2 1 4 3 1 2 1
 ```
+7 is the length of the array
+
+2 1 4 3 1 2 1 is the actual array
--- sleepsort.go
+++ sleepsort.go
@@ -7,23 +7,35 @@ import (
 	"sync"
 	"time"
 )
+var size int
+var wg sync.WaitGroup
 
-func sleepsort(n int) {
+func sleepsort(n int) int {
+	defer wg.Done()
 	time.Sleep(time.Duration(n) * time.Second)
-	fmt.Println(n)
+	return n
 }
 
+func printer(in <-chan int) {
+	for i := 0; i < size; i++ {
+		fmt.Print(<-in," ")
+	}
+}
+
+type Chan chan int
+
 func main() {
+	size, _  = strconv.Atoi(os.Args[1])
+	c := make(Chan, size)
 
-	var wg sync.WaitGroup
-
-	for _, n := range os.Args[1:] {
+	for _, n := range os.Args[2:] {
 		n, _ := strconv.Atoi(n)
 		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			sleepsort(i)
-		}(n)
+		go func(c Chan, i int) {
+			c <- sleepsort(i)
+		}(c, n)
 	}
 	wg.Wait()
+	printer(c)
+	close(c)
 }
